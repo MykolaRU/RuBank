85319d67321b648e3639b7c33c242f97d48c17be
Kriptopoliss <ethandivine@gmail.com>
2023-10-16 21:02:07 -0400

    Added functionality

---
+++ RuBankProject/src/AccountDatabaseTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AccountDatabaseTest {
+
+    @Test
+    void accountInDatabase() {
+        Checking testChecking1 = new Checking(new Profile("Ethan","DiVine",new Date("12/25/2000")),100);
+        Checking testChecking2 = new Checking(new Profile("Ethan","DiVine",new Date("12/25/2000")),100);
+        AccountDatabase accountDatabase = new AccountDatabase();
+        accountDatabase.open(testChecking1);
+        assertTrue(accountDatabase.close(testChecking2));
+    }
+
+    @Test
+    void accountNotInDatabase() {
+        Checking testChecking1 = new Checking(new Profile("Ethan","DiVine",new Date("12/25/2000")),100);
+        Checking testChecking2 = new Checking(new Profile("Grethan","BroVine",new Date("12/24/2000")),100);
+        AccountDatabase accountDatabase = new AccountDatabase();
+        accountDatabase.open(testChecking1);
+        assertFalse(accountDatabase.close(testChecking2));
+    }
+}
